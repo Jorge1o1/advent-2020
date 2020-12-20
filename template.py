7256910a09e3df10200cfd5bc44355b939049614
Jorge Martinez <jlmartinez@acropolismarketing.com>
2020-12-20 18:25:07 -0500

    Template update

--- template.py
+++ template.py
@@ -1,7 +1,11 @@
 def part1(data):
     pass
 
+def part2(data):
+    pass
+
 if __name__ == "__main__":
-    with open("day1-input", "r") as fp:
+    with open("day1.txt", "r") as fp:
         data = fp.read().splitlines()
         part1(data)
+        part2(data)
